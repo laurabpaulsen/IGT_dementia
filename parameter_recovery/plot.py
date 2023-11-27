4028c7ed5603d4f0276afae89858e17c7e0db8f3
laurabpaulsen <202005791@post.au.dk>
2023-11-27 20:24:21 +0100

    Plots on two rows

--- parameter_recovery/plot.py
+++ parameter_recovery/plot.py
@@ -33,29 +33,22 @@ def plot_recoveries(trues:list, estimateds:list, parameter_names:list, savepath:
     -------
     None
     """
-
-     # plot true vs estimated parameters
-    fig, axes = plt.subplots(1, len(trues), figsize = (15, 5))
+    fig, axes = plt.subplots(2, len(trues) // 2 + (len(trues) % 2 > 0), figsize = (10, 7), dpi = 300)
     
-    for true, estimated, parameter_name, axis in zip(trues, estimateds, parameter_names, axes):
+    for true, estimated, parameter_name, axis in zip(trues, estimateds, parameter_names, axes.flatten()):
         plot_recovery_ax(axis, true, estimated, parameter_name)
 
+    # if any of the axes is empty, remove it
+    for axis in axes.flatten():
+        if not axis.get_title():
+            fig.delaxes(axis)
+    
     plt.tight_layout()
     
     if savepath:
         plt.savefig(savepath)
 
 
-
-"""
-    # plot the recovery of the parameters
-    plot_recoveries(
-        trues = [mu_a_rew_t, mu_a_pun_t, mu_K_t, mu_omega_f_t, mu_omega_p_t],
-        estimateds = [mu_a_rew_e, mu_a_pun_e, mu_K_e, mu_omega_f_e, mu_omega_p_e],
-        parameter_names = ["a_pun", "a_rew", "K", "omega_f", "omega_p"],
-        savepath = savepath_fig / "hierachical_parameter_recovery_ORL.png"
-    )
-"""
 
 if __name__ == "__main__":
     path = Path(__file__).parent
@@ -111,7 +104,8 @@ if __name__ == "__main__":
     plot_recoveries(
         trues = [mu_a_rew_t, mu_a_pun_t, mu_K_t, mu_omega_f_t, mu_omega_p_t],
         estimateds = [mu_a_rew_e, mu_a_pun_e, mu_K_e, mu_omega_f_e, mu_omega_p_e],
-        parameter_names = ["a_pun", "a_rew", "K", "omega_f", "omega_p"],
+        parameter_names=["mu_a_pun", "mu_a_rew", "mu_K", "mu_omega_f", "mu_omega_p"],
+        #parameter_names = [r"$\mu A_{rew}$", r"$\mu A_{pun}$", r"$\mu K$", r"$\mu \omega_f$", r"$\mu \omega_p$"],
         savepath = fig_path / "hierachical_parameter_recovery_ORL.png"
     )
 
@@ -139,11 +133,11 @@ if __name__ == "__main__":
 
         for sub in unique_subjects:
             data_tmp_sub = data_tmp[data_tmp["sub"] == sub]
-            a_rew_t.append(data_tmp_sub["sub_a_rew"].unique()[0])
-            a_pun_t.append(data_tmp_sub["sub_a_pun"].unique()[0])
-            K_t.append(data_tmp_sub["sub_K"].unique()[0])
-            omega_f_t.append(data_tmp_sub["sub_omega_f"].unique()[0])
-            omega_p_t.append(data_tmp_sub["sub_omega_p"].unique()[0])
+            a_rew_t.append(data_tmp_sub["sub_a_rew"].iloc[0])
+            a_pun_t.append(data_tmp_sub["sub_a_pun"].iloc[0])
+            K_t.append(data_tmp_sub["sub_K"].iloc[0])
+            omega_f_t.append(data_tmp_sub["sub_omega_f"].iloc[0])
+            omega_p_t.append(data_tmp_sub["sub_omega_p"].iloc[0])
             
             a_rew_e.append(data_tmp_est[f"a_rew.{sub}"].mean())
             a_pun_e.append(data_tmp_est[f"a_pun.{sub}"].mean())
@@ -155,6 +149,7 @@ if __name__ == "__main__":
     plot_recoveries(
         trues = [a_rew_t, a_pun_t, K_t, omega_f_t, omega_p_t],
         estimateds = [a_rew_e, a_pun_e, K_e, omega_f_e, omega_p_e],
+        #parameter_names = [r"$A_{rew}$", r"$A_{pun}$", r"$K$", r"$\omega_f$", r"$\omega_p$"],
         parameter_names = ["a_pun", "a_rew", "K", "omega_f", "omega_p"],
         savepath = fig_path / "hierachical_parameter_recovery_ORL_individual.png"
     )
