463563d6294aa9e9f164fc36c62615e12fe42483
laurabpaulsen <202005791@post.au.dk>
2023-12-03 16:18:37 +0100

    add plots (for now just using simulated data!)

--- estimation/plot.py
+++ estimation/plot.py
@@ -1,9 +1,65 @@
 import pandas as pd
 from pathlib import Path
 from statistics import mode
-from scipy.stats import binom
+import numpy as np
 
 # local imports
 import sys
 sys.path.append(str(Path(__file__).parents[1]))
-from plot_fns import plot_recoveries, plot_descriptive_adequacy
+from utils.plotting import plot_posteriors_violin, plot_descriptive_adequacy
+from utils.helper_functions import chance_level
+
+
+if __name__ in "__main__":
+    path = Path(__file__).parent
+
+    # load posterior and behavioral data
+    inpath = path / "fit" / "param_est_HC_AD.csv"
+    est_data = pd.read_csv(inpath)
+
+    AD_data = pd.read_csv(path / "data" / "AD" / "data_AD_all_subjects.csv")
+    HC_data = pd.read_csv(path / "data" / "HC" / "data_HC_all_subjects.csv")
+    HC_data["sub"] += AD_data["sub"].max() 
+
+    data = pd.concat([AD_data, HC_data])
+
+    outpath = path / "fig"
+
+    if not outpath.exists():
+        outpath.mkdir(parents = True)
+
+
+    # plot the posterior densities of the parameters
+    parameters = ["delta_a_rew", "delta_a_pun", "delta_K", "delta_omega_p", "delta_omega_f"]
+    densities = [np.array(est_data[parameter]) for parameter in parameters]
+    parameter_names = ["$\Delta A_{rew}$", "$\Delta A_{pun}$", "$\Delta K$", "$\Delta \omega_{p}$", "$\Delta \omega_{f}$"]
+
+    plot_posteriors_violin(densities, parameter_names, savepath = outpath / "posterior_densities.png")
+
+    pred_choices = []
+    # get the predicted choices
+    for sub in range(1, 41):
+        pred_choices_sub = []
+        
+        for t in range(1, 101):
+            pred_choices_sub.append(mode(est_data[f"y_pred.{sub}.{t}"]))
+
+        pred_choices.append(pred_choices_sub)
+
+    # get the actual choices 
+    choices = []
+    for sub in data["sub"].unique():
+        tmp_data = data[data["sub"] == sub]
+        choices_sub = data["choice"].to_list()
+
+        choices.append(choices_sub)
+
+    
+    # plot the descriptive adequacy of the model
+    plot_descriptive_adequacy(
+        choices, 
+        pred_choices, 
+        groups = [1] * AD_data["sub"].nunique()+ [2] * HC_data["sub"].nunique(),
+        chance_level = chance_level(n = 100, p = 0.25),
+        savepath = outpath / "descriptive_adequacy.png"
+        )
