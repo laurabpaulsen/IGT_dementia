076b23d729bd5d2873ddea2da95202aad51a4107
laurabpaulsen <202005791@post.au.dk>
2023-12-05 11:13:24 +0100

    move function for fitting model to utils

--- recovery/recover_subj_lvl.py
+++ recovery/recover_subj_lvl.py
@@ -3,40 +3,11 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-
-def recover(data, model_spec, savepath = None):
-    """
-    Generate synthetic data and fit the model to it. Check how well the parameters are recovered by plotting median against the true parameters.
-
-    Parameters
-    ----------
-    
-    model_spec : str
-        Stan model specification.
-    savepath : Path, optional
-        Path to save the fitted parameters to. The default is None.
-    """
-
-    data_dict = {
-        "choice" : np.array(data["choice"]).astype(int),
-        "outcome" : np.array(data["outcome"]),
-        "sign_out" : np.array(data["sign_out"]),
-        "T": int(len(data)), #total number of trials
-    }
-
-    # fit the model
-    model = stan.build(model_spec, data = data_dict)
-    fit = model.sample(
-        num_chains = 4, 
-        num_samples = 2000,
-        num_warmup = 1000)
-
-    # get the estimated parameters
-    df = fit.to_frame()
-
-    # save the data
-    if savepath:
-        df.to_csv(savepath, index = False)
+# local imports 
+import sys
+sys.path.append(str(Path(__file__).parents[1]))
+from utils.fit_model import fit_subject_level
+from utils.helper_functions import parse_n_subj
 
 
 
@@ -52,7 +23,7 @@ if __name__ == "__main__":
         model_spec = f.read()
 
     # load the simulated data
-    n_subs = 100
+    n_subs = parse_n_subj()
     sim_path = path / "simulated" / "subj_lvl" / f"ORL_{n_subs}_sub.csv"
 
     data = pd.read_csv(sim_path)
@@ -60,7 +31,7 @@ if __name__ == "__main__":
     for sub in range(1, n_subs + 1):
         tmp_data = data[data["sub"]==sub]
     
-        recover(
+        fit_subject_level(
             data = tmp_data,
             model_spec = model_spec,
             savepath = outpath / f"param_rec_subj_{sub}.csv"
--- utils/fit_model.py
+++ utils/fit_model.py
@@ -52,3 +52,39 @@ def fit_group_level(data, model_spec, savepath = None):
     # save the data
     if savepath:
         df.to_csv(savepath, index = False)
+
+
+
+def fit_subject_level(data, model_spec, savepath = None):
+    """
+    Generate synthetic data and fit the model to it. Check how well the parameters are recovered by plotting median against the true parameters.
+
+    Parameters
+    ----------
+    
+    model_spec : str
+        Stan model specification.
+    savepath : Path, optional
+        Path to save the fitted parameters to. The default is None.
+    """
+
+    data_dict = {
+        "choice" : np.array(data["choice"]).astype(int),
+        "outcome" : np.array(data["outcome"]),
+        "sign_out" : np.array(data["sign_out"]),
+        "T": int(len(data)), #total number of trials
+    }
+
+    # fit the model
+    model = stan.build(model_spec, data = data_dict)
+    fit = model.sample(
+        num_chains = 4, 
+        num_samples = 1000,
+        num_warmup = 1000)
+
+    # get the estimated parameters
+    df = fit.to_frame()
+
+    # save the data
+    if savepath:
+        df.to_csv(savepath, index = False)
