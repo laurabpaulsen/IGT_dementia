eb294888444a3c4d4220f855923d9be94f855cd7
laurabpaulsen <202005791@post.au.dk>
2023-12-05 11:13:05 +0100

    add parser for number of subjects

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,5 +1,19 @@
 from scipy.stats import binom
 import numpy as np
+import argparse
+
+def parse_n_subj():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n_subj",
+        "-n",
+        type = int,
+        default = 10,
+        help = "The number of subjects to simulate, recover or plot depending on the script."
+    )
+    args = parser.parse_args()
+    return args.n_subj
+
 
 def logit(x):
     return np.log(x / (1 - x))
