86e828700ae6f14030a44d458bb6df8e46ee057e
laurabpaulsen <202005791@post.au.dk>
2024-01-04 18:23:18 +0100

    always fix theta at 1

--- recovery/recover_subj_lvl.py
+++ recovery/recover_subj_lvl.py
@@ -17,30 +17,22 @@ if __name__ == "__main__":
     n_subs = parse_n_subj()
 
     outpath = path / "fit" / "subj_lvl"
+    outpath.mkdir(parents=True, exist_ok=True)
 
+    sim_path = path / "simulated" / "subj_lvl" / f"{n_subs}" / f"ORL.csv"
+    model_path = path.parent / "models" / "IGT_ORL.stan"
 
-    for theta_bool, folder in zip([True, False], [f"{n_subs}", f"{n_subs}_no_theta" ]):
-        # make sure the output path exists
-        (outpath / folder).mkdir(parents=True, exist_ok=True)
-
-        sim_path = path / "simulated" / "subj_lvl" / f"{n_subs}"/ f"ORL.csv"
-
-        data = pd.read_csv(sim_path)
-
-        if theta_bool:
-            model_path = path.parent / "models" / "IGT_ORL.stan"
-        else:
-            model_path = path.parent /  "models" / "IGT_ORL_no_theta.stan"
-
-        with open(model_path, "r") as file:
-            model_spec = file.read()
-
-        for sub in range(1, n_subs + 1):
-            tmp_data = data[data["sub"]==sub]
+    data = pd.read_csv(sim_path)
         
-            fit_subject_level(
-                data = tmp_data,
-                model_spec = model_spec,
-                savepath = outpath / folder / f"param_rec_subj_{sub}.csv"
-                )
-            print(f"Finished subject {sub} of {n_subs}")
+    with open(model_path, "r") as file:
+        model_spec = file.read()
+
+    for sub in range(1, n_subs + 1):
+        tmp_data = data[data["sub"]==sub]
+        
+        fit_subject_level(
+            data = tmp_data,
+            model_spec = model_spec,
+            savepath = outpath / f"param_rec_subj_{sub}.csv"
+        )
+        print(f"Finished subject {sub} of {n_subs}")
